606e6ba6c681cedbc74ed359edc6fe5e9f619daa
AbdurRahman <arshadhinjr@gmail.com>
2023-10-22 14:11:31 +0600

    mod: modal profile

--- src/Components/ProfileUploadModal/ProfileUploadModal.jsx
+++ src/Components/ProfileUploadModal/ProfileUploadModal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 
 import Button from '../Button/Button';
 import DivCenter from './../DivCenter/DivCenter';
@@ -5,7 +6,7 @@ import { RxCross2 } from 'react-icons/rx';
 
 const ProfileUploadModal = (props) => {
   return (
-    <div className='absolute bg-black bg-opacity-0 w-full h-screen top-0 left-0 z-40'>
+    <div className='absolute bg-black bg-opacity-0 w-full h-screen top-0 left-0 z-40'> 
       <div onClick={props.onClick} className='absolute bg-black bg-opacity-40 w-full h-screen top-0 left-0 z-30'></div>
       <DivCenter className=" md:w-[650px] w-[350px] bg-white rounded-3xl p-6 z-40">
         <span onClick={props.onClick} className='absolute right-4 top-2 text-xl p-1 text-gray-100 cursor-pointer active:scale-75 transform transition-all duration-200 bg-slate-900 rounded-full'><RxCross2></RxCross2></span>
