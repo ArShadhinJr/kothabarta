fe25833807ada34906914cdac441c2b179d87d77
AbdurRahman <arshadhinjr@gmail.com>
2023-11-27 23:38:58 +0600

    block and unblock is working

--- src/Components/Friend/Friend.jsx
+++ src/Components/Friend/Friend.jsx
@@ -1,7 +1,7 @@
 import Box from "../Box/Box"
 // import { frienData } from "../../assets/Data/FriendData"
 import Inner from "../Inner/Inner"
-import { getDatabase, onValue, ref, update } from "firebase/database"
+import { get, getDatabase, onValue, ref, update } from "firebase/database"
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 
@@ -27,30 +27,39 @@ const Friend = () => {
     
     } )
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ frienDataList ] )
+  }, [ db, userInformation, frienDataList ] )
   
-  const handleBlcok = (item) =>{
-    // block friend update status blocked with firebase
-    const userRef = ref(db, 'friendRequests/');
-    onValue(userRef, (snapshot) => {
+const handleBlock = async (item) => {
+  const userRef = ref(db, 'friendRequests/');
+
+  try {
+    const snapshot = await get(userRef);
+
+    const updatePromises = [];
+
     snapshot.forEach((user) => {
-      if ( user.val().senderId === item.senderId && user.val().receiverId === userInformation.email ) {
-        update(ref(db, 'friendRequests/' + user.key ), {
-          status: "blocked", 
-          blockedBy: userInformation.email, 
+      if (user.val().senderId === item.senderId && user.val().receiverId === userInformation.email) {
+        updatePromises.push(update(ref(db, 'friendRequests/' + user.key), {
+          status: "blocked",
+          blockedBy: userInformation.email,
           blockedId: user.val().senderId
-        })
-      } else if ( user.val().senderId === userInformation.email && user.val().receiverId === item.receiverId ) {
-        update(ref(db, 'friendRequests/' + user.key ), {
-          status: "blocked", 
-          blockedBy: userInformation.email, 
+        }));
+      } else if (user.val().senderId === userInformation.email && user.val().receiverId === item.receiverId) {
+        updatePromises.push(update(ref(db, 'friendRequests/' + user.key), {
+          status: "blocked",
+          blockedBy: userInformation.email,
           blockedId: user.val().receiverId
-        })
+        }));
       }
-    })
-    
     });
+
+    // Wait for all update operations to complete
+    await Promise.all(updatePromises);
+
+  } catch (error) {
+    console.error("Error fetching/updating Firebase:", error);
   }
+};
 
   return (
     <Box name="Friend">
@@ -59,7 +68,7 @@ const Friend = () => {
                 return (
                     <Inner key={index} src={ userInformation.photoURL=== item.photoURL  ? item.senderPhotoURL : item.photoURL} name={userInformation.displayName === item.receverName ? item.senderName : item.receverName } dec={userInformation.email === item.receiverId ? item.senderId : item.receiverId}>
                     {/* user block button */}
-                    <button onClick={()=>handleBlcok(item)} className=" bg-primary text-white px-5 py-1 rounded-lg active:scale-95">Block</button>
+                    <button onClick={()=>handleBlock(item)} className=" bg-primary text-white px-5 py-1 rounded-lg active:scale-95">Block</button>
                     </Inner>
                 )
             })
--- src/Components/Unblock/Unblock.jsx
+++ src/Components/Unblock/Unblock.jsx
@@ -1,61 +1,70 @@
-import Box from "../Box/Box"
-// import { unblockUsersData } from "../../assets/Data/Unblock"
-import Inner from "../Inner/Inner"
-import { useSelector } from "react-redux"
-import { useEffect, useState } from "react"
-import { getDatabase, onValue, ref, update } from "firebase/database"
+import Box from "../Box/Box";
+import Inner from "../Inner/Inner";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { get, getDatabase, onValue, ref, update } from "firebase/database";
 
 const Unblock = () => {
-const db = getDatabase();
-  const [ blockDataList, setBlockDataList ] = useState( [] )
-  const userInformation = useSelector( state => state.user.userInfo )
+  const db = getDatabase();
+  const [blockDataList, setBlockDataList] = useState([]);
+  const userInformation = useSelector((state) => state.user.userInfo);
 
-  useEffect( () => {
-    const userRef = ref( db, 'friendRequests/' );
-    onValue( userRef, ( snapshot ) => {
-      const blockData = []
-      snapshot.forEach( ( block ) => {
-        if ( (block.val().status === "blocked" && block.val().blockedBy === userInformation.email) || (block.val().status === "blocked" && block.val().blockedBy === userInformation.email) ) {
-          blockData.push( block.val() )
-        }
-      } )
-      setBlockDataList( blockData )
-    
-    } )
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ blockDataList ] )
-
-  const handleUnblcok = ( item) =>{
+  useEffect(() => {
     const userRef = ref(db, 'friendRequests/');
-    onValue(userRef, (snapshot) => {
+
+    const fetchData = () => {
+      onValue(userRef, (snapshot) => {
+        const blockData = [];
+        snapshot.forEach((block) => {
+          if ((block.val().status === "blocked") && (block.val().blockedBy === userInformation.email)) {
+            blockData.push(block.val());
+          }
+        });
+        setBlockDataList(blockData);
+      });
+    };
+
+    fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [db, userInformation, blockDataList]); // Include only the dependencies needed for the effect
+
+const handleUnblock = async (item) => {
+  const userRef = ref(db, 'friendRequests/');
+
+  try {
+    const snapshot = await get(userRef);
+
+    const updatePromises = [];
+
     snapshot.forEach((user) => {
-      if ( (user.val().status === "blocked") &&  (user.val().blockedBy === userInformation.email) && (user.val().blockedId === item.receiverId || user.val().blockedId === item.senderId ) ) {
-        update(ref(db, 'friendRequests/' + user.key ), {
-          status: "accept", 
-          blockedBy: "", 
-          blockedId: ""
-        })
+      if ((user.val().status === "blocked") && (user.val().blockedBy === userInformation.email) && (user.val().blockedId === item.receiverId || user.val().blockedId === item.senderId)) {
+        updatePromises.push(update(ref(db, 'friendRequests/' + user.key), {
+          status: "accept",
+          blockedBy: null,
+          blockedId: null
+        }));
       }
-    })
-    
     });
+
+    // Wait for all update operations to complete
+    await Promise.all(updatePromises);
+  } catch (error) {
+    console.error("Error fetching/updating Firebase:", error);
   }
+};
 
 
 
   return (
     <Box name="Block Users">
-        {
-            blockDataList.map((item, index)=>{
-                return (
-                  <Inner key={index} src={item.photoURL} name={item.receverName} dec={item.receiverId} >
-                    <button onClick={() => handleUnblcok( item )} className=" bg-primary text-white px-5 py-1 rounded-lg active:scale-95">Unblock</button>
-                  </Inner>
-                )
-            })
-        }
+      {blockDataList.map((item, index) => (
+        <Inner key={index} src={item.photoURL} name={item.receverName} dec={item.receiverId}>
+          <button onClick={() => handleUnblock(item)} className="bg-primary text-white px-5 py-1 rounded-lg active:scale-95">Unblock</button>
+        </Inner>
+      ))}
     </Box>
-  )
-}
+  );
+};
 
-export default Unblock
+export default Unblock;
