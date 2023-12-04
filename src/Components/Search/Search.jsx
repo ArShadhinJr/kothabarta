4b55d675a4a6b62d465697b940d04c46e452c944
AbdurRahman <arshadhinjr@gmail.com>
2023-12-05 00:29:06 +0600

    sol some isshus

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -3,7 +3,7 @@ import {BiDotsVerticalRounded}  from "react-icons/bi"
 
 const Search = () => {
   return (
-    <div className={`flex items-center justify-between border rounded-[20px] px-[23px] py-[20px] bg-white drop-shadow-l md:mt-0 mt-[54px] mb-[20px]`}>
+    <div className={`flex items-center justify-between border rounded-[20px] px-[23px] py-[20px] bg-white drop-shadow-l md:mt-0 mt-[40px] mb-[20px]`}>
         <div className="flex items-center">
             <span className="text-2xl pr-[32px]"><AiOutlineSearch></AiOutlineSearch></span>
             <input type="text" placeholder="search" className="outline-none focus:outline-none"/>
--- src/Components/SettingBox/SettingBox.jsx
+++ src/Components/SettingBox/SettingBox.jsx
@@ -8,13 +8,13 @@ const SettingBox = () => {
     const userInformation = useSelector(state => state.user.userInfo)
 
   return (
-    <div className='flex md:flex-row flex-col md:gap-x-8 md:h-11/12'>
-       <div className="md:w-1/2 md:h-full overflow-hidden bg-white border rounded-[20px] px-[40px] py-[10px] drop-shadow-lg">
+    <div className='flex md:flex-row flex-col  md:gap-x-8 gap-y-8 md:h-11/12'>
+       <div className="md:w-1/2 md:h-full overflow-hidden bg-white border rounded-[20px] md:px-[40px] px-[20px] py-[10px] drop-shadow-lg">
             <div className="h-1/12 flex items-center">
                 <h3 className='text-xl font-bold text-black'>Profile Settings</h3>
             </div>
-              <div className=' h-11/12 px-[42px] '>
-                <div className="flex items-center gap-x-[20px]">
+              <div className=' md:h-11/12 md:px-[42px] px-[10px]'>
+                <div className="flex items-center gap-x-[20px] md:mt-0 mt-[20px]">
                         <div >
                             <img src={userInformation.photoURL} className= {`rounded-full  md:w-[100px] w-[60px]`} />
                         </div>
@@ -23,13 +23,9 @@ const SettingBox = () => {
                             <p className="md:text-xl sm-text-base text-sm text-gray-500">Stay tuned</p>
                         </div>
                     </div>
-                <hr className='my-[23px]'/>
-                <div className='px-[40px] pt-[20px] pb-[43px]'>
+                <hr className='md:my-[23px]'/>
+                <div className='md:px-[40px] px-[15px] pt-[20px] md:pb-[43px] pb-[20px]'>
                     <ul className='flex flex-col gap-y-4'>
-                        {/* <SettingItem icon={<MdDriveFileRenameOutline />} name="Edit Profile Name." />
-                        <SettingItem icon={<MdComment />} name="Edit Profile Status Info." />
-                        <SettingItem icon={<MdAddPhotoAlternate />} name="Help." />
-                        <SettingItem icon={<MdOutlineHelpOutline />} name="Edit Profile Photo." /> */}
                           <SettingItem icon={<MdDriveFileRenameOutline />} name="Edit Profile Name." />
                           <SettingItem icon={<MdComment />} name="Edit Profile Status Info." />
                           <SettingItem icon={<MdAddPhotoAlternate />} name="Edit Profile Photo." />
@@ -40,11 +36,11 @@ const SettingBox = () => {
                 
             </div>
         </div>
-          <div className="md:w-1/2 md:h-full overflow-hidden bg-white border rounded-[20px] px-[40px] py-[10px] drop-shadow-lg">
+          <div className="md:w-1/2 md:h-full overflow-hidden bg-white border rounded-[20px] md:px-[40px] px-[20px] py-[10px] drop-shadow-lg">
               <div className="h-1/12 flex items-center">
                 <h3 className='text-xl font-bold text-black'>Account Settings</h3>
             </div>
-              <div className='px-[40px] pt-[20px] pb-[43px]'>
+              <div className='md:px-[40px] px-[15px] pt-[20px] md:pb-[43px] pb-[20px]'>
                     <ul className='flex flex-col gap-y-4'>
                       <SettingItem icon={<MdKey />} name="Change Password" />
                       <SettingItem icon={<MdOutlineIncompleteCircle />} name="Theme." />
